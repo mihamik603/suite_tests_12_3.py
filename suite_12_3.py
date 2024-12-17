f1d9e8dd9c083bbab33fe62c57723bd5820be949
mihamik603 <vmm1331@gmail.com>
2024-12-17 20:21:48 +0400

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,11 +1,9 @@
 import unittest
 from tests_12_3 import RunnerTest, TournamentTest
 
-# Создание тестового набора
 test_suite = unittest.TestSuite()
 test_suite.addTest(unittest.makeSuite(RunnerTest))
 test_suite.addTest(unittest.makeSuite(TournamentTest))
 
-# Запуск тестов
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(test_suite)
